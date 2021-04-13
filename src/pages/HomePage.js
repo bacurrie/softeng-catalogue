aef51f88d71e79885c213f725286409ab7a4d36f
LorynLosier <43613465+LorynLosier@users.noreply.github.com>
2021-04-12 22:08:00 -0300

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,33 @@
 import React, { useEffect, useState, Component } from "react";
+import bigRolo from '../data/images/bigRolo.png';
 // import axios from 'axios';
 
 function HomePage({ user }: { user: any }) {
   // const [message, setMessage] = useState('');
+  let mrolo = "Meet Rolo, He'll help you get back to the homepage!";
   let message;
+  let message2;
   //if user is set
 
   useEffect(() => {
   }, [user]);
   if (user) {
     message = `Hi ${user}`;
+    message2 = "";
   } else {
-    message = "You are not logged in!";
-    // all catalogues
+    message = "Please log in to see our catalogs!";
+    message2 = "If you are new you'll have to register.";
   }
   // setMessage(`Hi ${user.first_name} ${user.last_name}`);
   // setMessage('You are not logged in!');
 
   return (
     <div className="homepage">
-      <h1>Hello World!</h1>
+      <h1>Welcome to Dex!</h1>
       <p>{message}</p>
+      <p>{message2}</p>
+      <img src={bigRolo}></img>
+      <p>{mrolo}</p>
     </div>
   );
 }
